954673a893de65ffdcebb1b93acdf13c10928312
esracam <esra.cam@stu.pirireis.edu.tr>
2025-05-25 01:03:23 +0300

    lokasyon için class eklendi

--- src/Main.java
+++ src/Main.java
@@ -15,3 +15,21 @@ public class Ucak {
         return marka + " " + model + " [" + seriNo + "] Kapasite: " + koltukKapasitesi;
     }
 }
+public class Lokasyon {
+    String ulke;
+    String sehir;
+    String havaalani;
+    boolean aktif;
+
+    public Lokasyon(String ulke, String sehir, String havaalani, boolean aktif) {
+        this.ulke = ulke;
+        this.sehir = sehir;
+        this.havaalani = havaalani;
+        this.aktif = aktif;
+    }
+
+    public String toString() {
+        return sehir + ", " + ulke + " - " + havaalani + (aktif ? " (Aktif)" : " (Pasif)");
+    }
+}
+
